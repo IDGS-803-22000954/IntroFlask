b99b49e3848cf45a1d708a78a98dc1a8df082458
IDGS-803-22000954 <79204@alumnos.utleon.edu.mx>
2025-01-31 11:24:29 -0600

    formularios

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app=Flask(__name__)
 @app.route('/')
 def index():
-    return "Hola aquisi o aquino!!!"
+    grupo="IDGS803"
+    lista=["Juan", "Pedro", "Mario"]
+    return render_template("index.html", grupo=grupo, lista=lista)
+
+@app.route("/OperasBas")
+def operasBas():
+    return render_template("OperasBas.html")
+
+@app.route("/resultado", methods=["GET","POST"])
+def resultado():
+    if request.method == "POST":
+        num1=request.form.get("n1")
+        num2=request.form.get("n2")
+        r=int(num1)+int(num2)
+        return render_template("OperasBas.html", num1=num1, num2=num2, r=r)
+
+@app.route("/ejemplo1")
+def ejemplo1():
+    return render_template("ejemplo1.html")
 
 @app.route("/hola")
 def hola():
@@ -29,6 +47,22 @@ def suma(n1,n2):
 @app.route("/default/<string:nom>")
 def func(nom='pedro'):
     return 'El nombre de Nom es '+nom
+
+@app.route("/form1")
+def form1():
+    return '''
+    <form>
+    <label>Nombre</label>
+    <input type"text" name="nombre" placeholder="Nombre">
+    </br>
+    <label>Nombre</label>
+    <input type"text" name="nombre" placeholder="Nombre">
+    </br>
+    <label>Nombre</label>
+    <input type"text" name="nombre" placeholder="Nombre">
+    </br>
+    </form>
+    '''
 
 if __name__ == '__main__':
     app.run(debug=True, port=8081)
