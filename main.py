873cabea6b4b0732b1d8863f574bbb5e3cee929e
IDGS-803-22000954 <79204@alumnos.utleon.edu.mx>
2025-02-13 22:57:52 -0600

    formulario y cine

--- main.py
+++ main.py
@@ -4,20 +4,68 @@ app=Flask(__name__)
 @app.route('/')
 def index():
     grupo="IDGS803"
-    lista=["Juan", "Pedro", "Mario"]
+    lista="Juan", "Pedro", "Mario"
     return render_template("index.html", grupo=grupo, lista=lista)
 
 @app.route("/OperasBas")
 def operasBas():
     return render_template("OperasBas.html")
 
+@app.route("/cinepolis")
+def cinepolis():
+    return render_template("cinepolis.html")
+
 @app.route("/resultado", methods=["GET","POST"])
 def resultado():
     if request.method == "POST":
+        m=request.form.get('calculadora')
         num1=request.form.get("n1")
         num2=request.form.get("n2")
-        r=int(num1)+int(num2)
-        return render_template("OperasBas.html", num1=num1, num2=num2, r=r)
+        if m == "suma":
+            r=int(num1)+int(num2)
+            t='suma'
+        if m == "resta":
+            r=int(num1)-int(num2)
+            t='resta'
+        if m == "multiplicacion":
+            r=int(num1)*int(num2)
+            t='multiplicacion'
+        if m == "division":
+            r=int(num1)/int(num2)
+            t='division'
+        return render_template("OperasBas.html", num1=num1, num2=num2, r=r, t=t)
+
+@app.route("/cine", methods=["GET","POST"])
+def cine():
+    if request.method == 'POST':
+        nombre=request.form.get("nombre")
+        ccompradores=request.form.get("ccompradores")
+        tarjeta=request.form.get("tarjeta")
+        cboletas=int(request.form.get("cboletas"))
+        if int(cboletas) <= (int(ccompradores)*int(7)):
+            if tarjeta == "no":
+                if int(cboletas) >= int(5):
+                    precio=(int(cboletas)*int(12))*float(0.85)
+                    r="Hola {}, tu precio es de {} pesos".format(nombre, precio)
+                elif int(cboletas) >=3:
+                    precio=(int(cboletas)*int(12))*float(0.90)
+                    r="Hola {}, tu precio es de {} pesos".format(nombre, precio)
+                else:
+                    precio=int(cboletas)*int(12)
+                    r="Hola {}, tu precio es de {} pesos".format(nombre, precio)
+            elif tarjeta == "si":
+                if int(cboletas) >= int(5):
+                    precio=((int(cboletas)*int(12))*float(0.75))
+                    r="Hola {}, tu precio es de {} pesos".format(nombre, precio)
+                elif int(cboletas) >= int(3):
+                    precio=((int(cboletas)*int(12))*float(0.80))
+                    r="Hola {}, tu precio es de {} pesos".format(nombre, precio)
+                else:
+                    precio=(int(cboletas)*int(12))*float(0.90)
+                    r="Hola {}, tu precio es de {} pesos".format(nombre, precio)
+        else:
+            r="La cantidad de boletas excede a la de compradores"
+        return render_template("cinepolis.html", r=r) 
 
 @app.route("/ejemplo1")
 def ejemplo1():
